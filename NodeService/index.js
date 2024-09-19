df827f3280c1241ad1a2fd5d5fed6d136b74134b
Sebastián Fernández <sebasr.ferval@estudiantec.cr>
2024-09-19 16:20:14 -0600

    [ADD] hello world

---
+++ NodeService/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  console.log(req.method); // Use the 'req' parameter to avoid the unused variable warning
+  res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
