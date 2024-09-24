a1d7522543f67214fb9123f5d39e5ff88caee307
Sebastián Fernández <sebasr.ferval@estudiantec.cr>
2024-09-24 16:37:09 -0600

    nuevo Update Branch

--- NodeService/updateBranch.sh
+++ NodeService/updateBranch.sh
@@ -1,1 +1,20 @@
-git pull
+#!/bin/bash
+SERVICE_NAME="updateService.service"
+cd /home/sebas/Escritorio/SO_Lab_Service/NodeService
+git fetch origin
+
+LOCAL_HASH=$(git rev-parse HEAD)
+REMOTE_HASH=$(git rev-parse origin/deployment)
+
+if ["$LOCAL_HASH" != "$REMOTE_HASH"]; then
+    echo "Updating branch"
+    git checkout deployment
+    git pull origin deployment
+
+    systemctl restart $SERVICE_NAME
+    echo "Branch updated"
+    sudo systemctl daemon-reload
+    sudo systemctl restart update-deployment.timer
+else
+    echo "Branch up to date"
+fi
