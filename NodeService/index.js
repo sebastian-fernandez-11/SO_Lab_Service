13c9cad8a54bfbc2e69a05a0a4e17ea9b8ef4c06
Sebastián Fernández <sebasr.ferval@estudiantec.cr>
2024-09-24 22:10:25 -0600

    Update index.js

--- NodeService/index.js
+++ NodeService/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 200;
+const port = 2000;
 
 app.get('/', (req, res) => {
   console.log(req.method); // Use the 'req' parameter to avoid the unused variable warning
