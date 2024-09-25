e0313b85432c56f2a1922bb91f88b7e257974a2a
Sebastián Fernández <sebasr.ferval@estudiantec.cr>
2024-09-25 15:10:28 -0600

    [UPDATE] updateBranch.sh FINAL

--- NodeService/updateBranch.sh
+++ NodeService/updateBranch.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-SERVICE_NAME="updateService.service"
-cd /home/sebas/Escritorio/SO_Lab_Service/NodeService
-git fetch origin
+SERVICE_NAME="update-branch.service"
+while true; do
+    cd /home/sebas/Escritorio/SO_Lab_Service/NodeService
+    git fetch origin
 
-LOCAL_COMMIT=$(git rev-parse HEAD)
-REMOTE_COMMIT=$(git rev-parse origin/Deployment)
+    LOCAL_COMMIT=$(git rev-parse HEAD)
+    REMOTE_COMMIT=$(git rev-parse origin/Deployment)
 
-if [ "$LOCAL_COMMIT" != "$REMOTE_COMMIT" ]; then
-    echo "Updating branch"
-    git pull origin Deployment
+    if [ "$LOCAL_COMMIT" != "$REMOTE_COMMIT" ]; then
+        echo "Updating branch"
+        git pull origin Deployment
+        echo "Branch updated"
+    else
+        echo "Branch up to date"
+    fi
+    sleep 30
+done
 
-    systemctl restart $SERVICE_NAME
-    echo "Branch updated"
-    sudo systemctl daemon-reload
-    sudo systemctl restart update-deployment.timer
-else
-    echo "Branch up to date"
-fi
