ffa1e6475135af5ee251d9a67160926d03ef8854
Sebastián Fernández <80862646+sebastian-fernandez-11@users.noreply.github.com>
2024-09-25 15:51:24 -0600

    Update index.js

--- NodeService/index.js
+++ NodeService/index.js
@@ -4,7 +4,7 @@ const port = 2000;
 
 app.get('/', (req, res) => {
   console.log(req.method); // Use the 'req' parameter to avoid the unused variable warning
-  res.send('Hello World!');
+  res.send('Hello World! Con cambio del service');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
